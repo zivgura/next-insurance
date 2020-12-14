359b5921038ad1588f7383f9e090fc0299de3a18
maydrori <maydrori100@gmail.com>
2020-12-14 15:09:10 +0200

    fix cors

--- server/server.js
+++ server/server.js
@@ -8,9 +8,15 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/movies', (req, res) => res.send(movies));
+app.get('/movies', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send(movies);
+});
 
-app.get('/movies/:id', (req, res) => res.send(movies.filter(movie => movie.id === req.params.id)));
+app.get('/movies/:id', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send(movies.filter(movie => movie.id === req.params.id));
+});
 
 app.listen(3000, function () {
     console.log(`app listening on port ${3000}!`);
